c17be3165239b41699aac24fec22ebcf6d4bb37e
Valerie Zhao <vzhao@uchicago.edu>
2020-06-06 19:36:42 -0500

    sbatch script for quadro

---
+++ run_stuff.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --mail-user=vzhao@uchicago.edu
+#SBATCH --mail-type=ALL
+#SBATCH --output=/home/vzhao/slurm/slurm_out/%j.%N.stdout
+#SBATCH --error=/home/vzhao/slurm/slurm_out/%j.%N.stderr
+#SBATCH --workdir=/scratch/vzhao/ttic31110-agwe
+#SBATCH --job-name=agwe_project
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=04:00:00
+#SBATCH --partition quadro
+#SBATCH --gres=gpu:1
+#SBATCH --mem=24000
+#SBATCH --exclusive
+
+pwd; hostname; date
+
+./train.sh
