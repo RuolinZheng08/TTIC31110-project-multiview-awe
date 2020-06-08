3991f2d8323359fd3f673fbf94094da192ca95d5
Valerie Zhao <vzhao@uchicago.edu>
2020-06-07 23:08:23 -0500

    bug fix for global_steps + obj02-phones-0.0001-0.4-0.5

--- lib/utils/main.py
+++ lib/utils/main.py
@@ -32,10 +32,10 @@ def tuneHyperparamsAndSave(config, small_grid_search=True):
     hyperparams = { # 4 combos, lr 0.0001 is too slow!!!
       'optim_lr' : [0.0001], # [0.001, 0.0001]
       'dropout' : [0.4],
-      'loss_margin': [0.5] # [0.4, 0.5]
+      'loss_margin': [0.4] # [0.4, 0.5]
     }
     # tally:
-    # obj02 phones: 0.001-0.4-0.4 and 0.001-0.4-0.5 have finished; 0.0001-0.4-0.5 in progress
+    # obj02 phones: 0.001-0.4-0.4/5 and 0.0001-0.4-0.5 have finished; 0.0001-0.4-0.4 in progress
     # obj02 chars:
     # obj0 phones: 0.001-0.4-0.4 started
     # obj0 chars: 0.001-0.4-0.4 started
@@ -55,7 +55,7 @@ def tuneHyperparamsAndSave(config, small_grid_search=True):
       config.net_view1_dropout = dropout
       config.net_view2_dropout = dropout
       config.loss_margin = margin
-      config.global_global_step = 0
+      config.global_step = 0
 
       log.info(f"Hyperparam tuning-------------------")
       log.info(f"optim_lr: {config.optim_lr}")
